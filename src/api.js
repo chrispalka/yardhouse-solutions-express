2468a653cbdf3dd83c9f332a7ba690cbeb03c428
Chris Palka <cpalka87@gmail.com>
2025-01-06 23:52:30 -0700

    update dependencies

--- src/api.js
+++ src/api.js
@@ -1,61 +1,62 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const nodemailer = require('nodemailer');
-const serverless = require('serverless-http')
+const serverless = require('serverless-http');
 
+const app = express();
 
-const app = express()
-
-app.use(cors())
+app.use(cors());
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({
-    'hello': 'hi!'
-  })
-})
-
+    hello: 'hi!',
+  });
+});
 
 router.post('/formSubmit', async (req, res) => {
   let body = JSON.parse(Buffer.from(req.body, 'base64').toString());
 
-  const { firstName, lastName, email, phone, message } = body.data
+  const { firstName, lastName, email, phone, message } = body.data;
+
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     name: 'www.gmail.com',
     auth: {
-      user: process.env.EMAIL_ADDRESS,
+      user: process.env.EMAIL_ADDRESS_FROM,
       pass: process.env.EMAIL_PASSWORD,
     },
   });
   const mailOptionsToDava = {
-    from: process.env.EMAIL_ADDRESS,
+    from: process.env.EMAIL_ADDRESS_FROM,
     to: process.env.EMAIL_ADDRESS_TO,
     subject: `New contact request from: ${firstName} ${lastName}`,
-    text: `Name: ${firstName} ${lastName}\nEmail: ${email}\n${phone !== '' ? 'Phone: ' + phone : ''}\n${message !== '' ? 'Message: \n\n' + message : ''}
-    `
+    text: `Name: ${firstName} ${lastName}\nEmail: ${email}\n${
+      phone !== '' ? 'Phone: ' + phone : ''
+    }\n${message !== '' ? 'Message: \n\n' + message : ''}
+    `,
   };
   const mailOptionsToRequester = {
-    from: process.env.EMAIL_ADDRESS,
+    from: process.env.EMAIL_ADDRESS_FROM,
     to: email,
     subject: `Auto-Reply From Dava Solutions`,
-    text: `Hello ${firstName},\n\nThank you for reaching out. \n\nA member of our team will get back to you as soon as possible. \n\nFor a faster response, you may text us at: (678) 424-7208.`
+    text: `Hello ${firstName},\n\nThank you for reaching out. \n\nA member of our team will get back to you as soon as possible. \n\nFor a faster response, you may text us at: (678) 424-7208.`,
   };
 
   Promise.all([
     transporter.sendMail(mailOptionsToDava),
-    transporter.sendMail(mailOptionsToRequester)
+    transporter.sendMail(mailOptionsToRequester),
   ])
     .then((response) => {
-      res.status(200).send('success')
-      console.log(response)
+      res.status(200).send('success');
+      console.log(response);
     })
     .catch((err) => {
-      res.status(404)
-      console.log(err)
-    })
+      res.status(404);
+      console.log(err);
+    });
 
   // transporter.sendMail(mailOptionsToRequester, (err, res) => {
   //   if (err) {
@@ -67,7 +68,7 @@ router.post('/formSubmit', async (req, res) => {
   // });
 });
 
-app.use('/.netlify/functions/api', router)
+app.use('/.netlify/functions/api', router);
 
-module.exports = app
-module.exports.handler = serverless(app)
+module.exports = app;
+module.exports.handler = serverless(app);
